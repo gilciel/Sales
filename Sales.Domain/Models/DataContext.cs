f50a7bf89413402afe5b44da919b0537003db88e
gilciel <gilcielruby@gmail.com>
2018-09-27 23:09:50 -0300

    Project Creation

---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Sales.Domain.Models
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using Common.Models;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+        public IQueryable<Product> Products { get; set; }
+    }
+}
